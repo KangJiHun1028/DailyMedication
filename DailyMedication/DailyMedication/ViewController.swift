19d4994bf3e83978a280d428ad394fb98b4bedcf
KangJiHun1028 <minsakr10@naver.com>
2023-12-12 14:57:46 +0900

    노스토리 보드 환경 구축

---
+++ DailyMedication/DailyMedication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DailyMedication
+//
+//  Created by t2023-m0053 on 12/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
